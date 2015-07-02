089cc9ff7be525d6bdb1244af1a62db3a2d35854
mrlong <mrlong.com@gmail.com>
2015-07-02 11:14:18 +0800

    ss

--- service/wechat.js
+++ service/wechat.js
@@ -130,9 +130,7 @@ exports.SCAN=function(event,fn){
     
     //有可能已关注了，但功能后开发，用户信息没有及时更新。
     Api.getUser(event.FromUserName,function(err,data){
-      if(!err){
-        donwloaduserheadimg(data);
-      }
+      if(!err){subscribe(data);}
     });
   }
   var content;
