af079996866c7845640b2d74acf0f5fa74056bf7
mrlong <mrlong.com@gmail.com>
2015-07-02 11:28:02 +0800

    ss

--- wechat/action/event.js
+++ wechat/action/event.js
@@ -22,7 +22,7 @@
 var User = require('../../service/wechat.js');
 
 module.exports = function(event, req, res, next){
-  console.log(event);
+  //console.log(event);
   //关注
   if (event.Event === 'subscribe') {
     User.getuser(event.FromUserName,function(err,data){
@@ -61,7 +61,7 @@ module.exports = function(event, req, res, next){
     
   }
   else if (event.Event =='CLICK'){
-    if(event.EventKey=='V1002'){
+    if(event.EventKey=='V1003'){
       res.reply("擎洲公司客服QQ竭诚为您服务：" +
                 "计价、招投标类软件：" +
                 "擎洲宝宝：755069890" +
@@ -70,7 +70,6 @@ module.exports = function(event, req, res, next){
                 "擎洲迎迎：755085748" +
                 "擎洲笑笑：188283969" +
                 "擎洲乐乐：1773929229" +
-
                 "算量类软件：" +
                 "擎洲妮妮：200825297 " +  
                 "擎洲宁宁：807656836 " +    
@@ -80,21 +79,51 @@ module.exports = function(event, req, res, next){
       );
     }
     else if(event.EventKey=='V1003'){
-      res.reply('你好，我是你的专门客服。'); 
+      
+      var msg="擎洲公司24小时服务热线" +
+      "计价及其他服务热线：400-001-9090" +
+      "算量产品服务热线：4000-166-166" +
+
+      "擎洲公司各地分公司联系方式：" +
+      "杭州一部（市区）：15382301050" +
+      "杭州二部（周边）：15382301052" +
+      "湖州大区：13388698257" +
+      "衢州大区：15397591692" +
+      "宁波舟山大区：15381888520" +
+      "嘉兴大区：15325370918" +
+      "金华绍兴大区：18067621219" +
+      "台州大区：15397139690" +
+      "温州丽水大区：13396959080"; 
+      res.reply(msg); 
     }
     else if(event.EventKey=='V1004'){
-      res.reply('你好，我是你的专门客服。');   
+      
+      var msg=  "擎洲公司客户QQ群，期待您的加入，若群满请联系服务QQ：" +
+        "杭州园林电子标群 136363576" +
+        "擎洲广达杭州群（周边） 103606096" +
+        "擎洲广达杭州群（市区） 416410783" +
+        "擎洲广达嘉兴群 25786132" +
+        "擎洲广达湖州群 79241524" +
+        "擎洲广达温州群 132966461" +
+        "擎洲广达丽水群 171922312" +
+        "擎洲广达台州群 428031087" +
+        "擎洲广达宁波群 341933607" +
+        "擎洲广达金华群 121886387" +
+        "擎洲广达绍兴群 23604988" +
+        "擎洲广达衢州群 82711220" +
+        "擎洲公路软件交流群 258613816" +
+        "擎洲水利计价群 46206438" +
+        "擎洲资料软件交流群 331285379" +
+
+      res.reply(msg);   
     }
     else{
-      res.reply('不要随意的点，现在还不支持。');
+      res.reply('现在还不开放该功能。');
     }
   }
   /////////////////////////////////////// 已关注之后，扫二维的事件//////////////////////////////////////
   else if(event.Event =='SCAN'){
-    console.log('aaa');
     User.SCAN(event,function(err,content){
-      console.log('111');
-      console.log(content);
       if(!err){
         res.reply(content); 
       }
