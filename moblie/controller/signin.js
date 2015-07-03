67991dd075f88b3bb2da5e7a8fc560321a799abf
mrlong <mrlong.com@gmail.com>
2015-07-03 11:57:29 +0800

    ss

--- moblie/controller/signin.js
+++ moblie/controller/signin.js
@@ -7,7 +7,37 @@ var Db = require('../../lib/db');
 router.get('/',function(req,res,next){
   var meet_guid = req.query.meet_guid;
   var openid = req.query.openid;
-  res.loadview('signin/index.html',{meet_guid:meet_guid});
+  
+  //有可能以前保存下来的电话，查users表。
+  
+  Db.query('select user_phone from users where openid=? ',openid,function(err,rows){
+    var myphonecode ;
+    if(!err && rows){
+        myphonecode = rows[0].user_phone;
+    };
+    res.loadview('signin/index.html',{meet_guid:meet_guid,openid:openid,
+                                      user_phone:myphonecode,user_phone_old:myphonecode});  
+  });
+  
+  
+});
+
+router.post('/',function(req,res,next){
+  
+  var openid = req.body.openid;
+  var meet_guid = req.body.meet_guid;
+  var user_phone = req.body.user_phone;
+  var user_phone_old = req.body.user_phone_old; //原来老的，如不一样则要写入库了。
+  
+  //写入库内并处理
+  
+  res.json({success:true,msg:meet_guid+user_phone+openid+user_phone_old});
+  
+  //电话不一样，写入user库
+  if(user_phone_old != user_phone){
+    
+  };
+
 });
 
 
