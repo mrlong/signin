571571bb31572c8eb750ce124c04870833499384
mrlong <mrlong.com@gmail.com>
2015-07-03 13:32:24 +0800

    ss

--- moblie/controller/signin.js
+++ moblie/controller/signin.js
@@ -9,8 +9,7 @@ router.get('/',function(req,res,next){
   var openid = req.query.openid;
   
   //有可能以前保存下来的电话，查users表。
-  
-  Db.query('select user_phone from users where openid=? ',openid,function(err,rows){
+  Db.query('select user_phone from users where user_openid=? ',openid,function(err,rows){
     var myphonecode ;
     if(!err && rows){
         myphonecode = rows[0].user_phone;
@@ -25,22 +24,35 @@ router.get('/',function(req,res,next){
 router.post('/',function(req,res,next){
   
   var openid = req.body.openid;
-  var meet_guid = req.body.meet_guid;
+  var meet_guid = 'qqq';// req.body.meet_guid;
   var user_phone = req.body.user_phone;
   var user_phone_old = req.body.user_phone_old; //原来老的，如不一样则要写入库了。
   
   //写入库内并处理
+  Db.query('update meeting_usr set meus_sginin=true,meus_openid=? where meet_guid=? and meus_phone=? and meus_sginin=false ',
+           [openid,meet_guid,user_phone],function(err,rows){
+    
+    if(!err){
+      if(rows.changedRows>0){
+        res.json({success:true,msg:"签到成功"});
+      }
+      else{
+        res.json({success:false,msg:'签到失败，怀疑无签到数据或已经签到。'});   
+      }
+    }
+    else{
+      res.json({success:false,msg:'服务器更新签到数据出错'});
+    }
   
-  res.json({success:true,msg:meet_guid+user_phone+openid+user_phone_old});
+  });
+  
   
   //电话不一样，写入user库
   if(user_phone_old != user_phone){
-    
+    Db.query('update users set user_phone=? where user_openid=? ',[user_phone,openid]);
   };
 
 });
-
-
 
 
 
