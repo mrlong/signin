9f41ba60031f1f12e0ee738401b5519ba49fe84c
mrlong <mrlong.com@gmail.com>
2015-07-14 11:22:09 +0800

    ss

--- admin/router-admin.js
+++ admin/router-admin.js
@@ -7,13 +7,16 @@ router.use(function(req,res,next){
   res.viewstyle = 'admin'; 
 
   
-  if(req.session.openid || (req.sessionStore.sessions[req.sessionID] && (req.sessionStore.sessions[req.sessionID].indexOf('openid')>0)) || req.originalUrl=='/admin/login'){
+  if(req.session.openid || (req.sessionStore.sessions[req.sessionID] && 
+                            (req.sessionStore.sessions[req.sessionID].indexOf('openid')>=0)) || req.originalUrl=='/admin/login'){
     next(); 
   }
   else{
      //next(); 
     //req.session.openid='6666';
     console.log(req.sessionStore);
+    console.log(req.sessionStore.sessions[req.sessionID]);
+    console.log(req.sessionStore.sessions[req.sessionID].indexOf('openid')>=0);
     console.log('session=' + req.session.openid);
     res.redirect('/admin/login'); 
   }
