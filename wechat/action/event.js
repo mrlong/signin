6075345358d103d3838ee7213dc23fea1c6ea1cc
mrlong <mrlong.com@gmail.com>
2015-07-14 09:09:06 +0800

    sss

--- wechat/action/event.js
+++ wechat/action/event.js
@@ -35,11 +35,7 @@ module.exports = function(event, req, res, next){
                 res.reply(content); 
               }
               else{
-                if(event.key == 'qrscene_100001'){
-                  res.reply( err + '(二维码号:' + event.EventKey + ')'  + 'openid=' + event.FromUserName);
-                }
-                else
-                  res.reply('无活动可参与2。(二维码号:' + event.EventKey + ')' + err); 
+                res.reply('无活动可参与。(二维码号:' + event.EventKey + ')' + err); 
               }
             });
           }
@@ -136,11 +132,11 @@ module.exports = function(event, req, res, next){
         res.reply(content); 
       }
       else{
-        if(event.key == 100001){
+        if(event.EventKey == 100001){
           res.reply( err + '(二维码号:' + event.EventKey + ')'  + 'openid=' + event.FromUserName);
         }
         else
-          res.reply('无活动可参与1。(二维码号:' + event.EventKey + ')' + err); 
+          res.reply('无活动可参与。(二维码号:' + event.EventKey + ')' + err); 
       }
     });
   }
